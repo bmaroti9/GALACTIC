d32d58c42a87d7b523d5afb26525f92d2dfde2d2
Balint Maroti <bmaroti9@gmail.com>
2022-05-29 19:45:48 -0500

    small change

--- network_helper.py
+++ network_helper.py
@@ -119,7 +119,7 @@ def spaceship_check(detailed_data):
     
     for n in OTHER_SPACESHIPS:
         if n.no_signal_alarm > 10:
-            del OTHER_SPACESHIPS[OTHER_SPACESHIPS.index(n)]
+            del OTHER_SPACESHIPS[OTHER_SPACESHIPS.sprites().index(n)]
         
 
 def passby_update(passby_data):
